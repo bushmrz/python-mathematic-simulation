881d830a41f3c34a47c071ade3243fd9d1377436
bushmrzzz <buuushmrz@mail.ru>
2023-02-16 23:12:08 +0300

    привет саня

---
+++ monte-karlo-method_1.py
@@ -0,0 +1,109 @@
+import matplotlib.pyplot as plt
+import random
+from matplotlib.widgets import TextBox
+
+arr_y21 = []
+arr_y22 = []
+arr_x = []
+
+#
+x_inside = []
+y_inside = []
+
+#
+x_outside = []
+y_outside = []
+
+n = 11
+x_max = 22
+y_max = 43
+M = S = 0
+expression = 100
+
+fig, ax = plt.subplots()
+
+
+def f(x):
+    result_1 = (10 * x) / n
+    result_2 = 10 * ((x - 20) / (n - 20)) + 20
+    return result_1, result_2
+
+
+def rand_point(x1, y1, x2, y2):
+    x = random.uniform(x1, x2)
+    y = random.uniform(y1, y2)
+    return x, y
+
+
+def submit(expression):
+    ax.clear()
+    draw_graph(arr_x, arr_y21,arr_y22)
+    arr_xx = []
+    arr_yy = []
+    for i in range(int(expression)):
+        x,y = rand_point(0,0,22,43)
+        arr_xx.append(x)
+        arr_yy.append(y)
+
+    draw_points(arr_xx, arr_yy)
+    M = len(x_inside)
+    print(M)
+    S = round((M / n) * (x_max * y_max), 2)
+    print(S)
+    fig.suptitle("Кол-во точек внутри треугольника = " + str(M) + "\nПлощадь треугольника методом Монте-Карло = " + str(S))
+    print(expression)
+    print("ez")
+
+
+def draw_points(arr_xx, arr_yy):
+    # по условию определяем точки внутри/снаружи фигуры
+    for i in range(0, expression):
+        f1, f2 = f(arr_xx[i])
+        if f1 < arr_yy[i] < f2:
+            x_inside.append(arr_xx[i])
+            y_inside.append(arr_yy[i])
+        else:
+            x_outside.append(arr_xx[i])
+            y_outside.append(arr_yy[i])
+
+    # строим точки на графике
+    ax.scatter(x_inside, y_inside, color='green')
+    ax.scatter(x_outside, y_outside, color='red')
+
+
+def draw_graph(arr_x, arr_y21, arr_y22):
+    ax.plot(arr_x, arr_y21, color='blue', label="Exp func")
+    ax.plot(arr_x, arr_y22, color='magenta', label="Quadro func")
+
+    ax.plot([0, 0], [0, arr_y22[0]], color='black')
+    #ax.plot([0, arr_x[-1]], [0, 0], [0, arr_x[-1]], [arr_y22[0], arr_y22[0]], color='green')
+
+
+def ex1():
+    n = 11
+    i = 0
+
+    arr_x.append(i)
+    arr_y21.append(10*arr_x[i]/n)
+    arr_y22.append(10*(arr_x[i]-20)/(n-20) + 20)
+
+    while abs(arr_y21[i] - arr_y22[i]) > 0.3:
+        i += 1
+        arr_x.append(i)
+        fx21 = 10*arr_x[i]/n
+        fx22 = 10*(arr_x[i]-20)/(n-20) + 20
+        arr_y21.append(fx21)
+        arr_y22.append(fx22)
+
+
+    axbox = fig.add_axes([0.1, 0, 0.8, 0.075])
+    text_box = TextBox(axbox, "Кол-во точек", textalignment="center")
+    text_box.on_submit(submit)
+    text_box.set_val(100)
+
+    #legend = plt.legend(loc='upper left', shadow=True, fontsize='x-small')
+    plt.show()
+
+
+ex1()
+
---
+++ monte-karlo-method_2.py
@@ -0,0 +1,97 @@
+import math
+import matplotlib.pyplot as plt
+import random
+from matplotlib.widgets import TextBox
+from scipy import integrate
+
+n_var = 11
+expression = 1000
+M=S=0
+x_max = 7
+y_max = 6
+#x_max = 8
+x_dots = []
+y_dots = []
+
+# точки внутри фигуры
+x_inside = []
+y_inside = []
+
+# точки вне фигуры
+x_outside = []
+y_outside = []
+
+v,err = 0, 0
+fig, ax = plt.subplots()
+
+x_gen = []
+y_gen = []
+
+def f(x):
+    #return math.sqrt(7 - 3 * math.sin(x) * math.sin(x))
+    return math.sqrt(29-n_var*(math.cos(x)**2))
+
+
+
+# for i in range(1,8):
+# #for i in range(1, 9):
+#     y_max = max(f(i), y_max)
+
+def find_points(expression):
+    for i in range(0, int(expression)):
+        if y_gen[i] < f(x_gen[i]):
+            x_inside.append(x_gen[i])
+            y_inside.append(y_gen[i])
+        else:
+            x_outside.append(x_gen[i])
+            y_outside.append(y_gen[i])
+
+
+def draw_graph():
+    ax.plot(x_dots, y_dots, '-', color='b')
+    #plt.ylim(0, y_max + 0.5)
+    ax.grid()
+
+
+def submit(expression):
+    ax.clear()
+    draw_graph()
+
+    for i in range(0, int(expression)):
+        x_gen.append(random.uniform(0, x_max))
+        y_gen.append(random.uniform(0, y_max))
+
+    find_points(expression)
+    ax.scatter(x_inside, y_inside,  color='green')
+    ax.scatter(x_outside, y_outside,  color='red')
+
+    M = len(x_inside)
+    S = round((M / int(expression)) * (x_max * y_max), 2)
+
+    v, err = integrate.quad(f, 0, x_max)
+
+    fig.suptitle('Кол-во точек внутри = ' + str(M) + '\nПлощадь фигуры методом Монте-Карло = ' + str(S)
+              + '\nТочная площадь фигуры = ' + str(v), fontsize=10)
+
+    print(expression)
+
+
+def ex2():
+    x = 0
+    while x <= x_max:
+        x_dots.append(x)
+        x += 0.1
+
+    for dot in x_dots:
+        y = f(dot)
+        y_dots.append(y)
+
+
+    axbox = fig.add_axes([0.1, 0, 0.8, 0.075])
+    text_box = TextBox(axbox, "Кол-во точек", textalignment="center")
+    text_box.on_submit(submit)
+    text_box.set_val(expression)
+
+    plt.show()
+
+ex2()
